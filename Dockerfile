2ca6f308da00a55b29413585f764ff94654e1800
Garth Pickell <garth.pickell@vertigis.com>
2022-06-09 14:17:05 -0700

    *** test

--- Dockerfile
+++ Dockerfile
@@ -2,8 +2,14 @@ FROM nginx
 RUN apt -y update
 RUN apt -y upgrade
 RUN apt -y install curl git procps
+RUN curl -fsSL https://deb.nodesource.com/setup_16.x | bash -
+RUN apt -y install nodejs
+RUN npm install yarn -g
 RUN rm -rf /etc/nginx
-RUN ln -sf /app/etc/nginx /etc/nginx
+WORKDIR /etc/nginx
+COPY ./etc/nginx/ ./
 WORKDIR /app
 COPY ./ ./
-
+WORKDIR /etc/nginx
+CMD node /app/src/main.mjs
+ENV REPO_URL https://github.com/gpickell/nginx-gw
--- src/main.mjs
+++ src/main.mjs
@@ -1,7 +1,69 @@
+import { spawn } from "child_process";
+import { stat } from "fs/promises";
 
+const { REPO_URL } = process.env;
+
+function exists() {
+    return stat(".git").then(x => x.isDirectory(), () => false);
+}
+
+async function shell(cmd) {
+    const child = spawn(cmd, {
+        shell: true,
+        stdio: "inherit",
+    });
+
+    const code = await new Promise((resolve, reject) => {
+        child.on("error", reject);
+        child.on("exit", code => resolve(!code));
+    });
+
+    return code;
+}
+
+async function boot() {
+
+}
+
+async function wait() {
+    return new Promise(() => {});
+}
+
+async function update() {
+
+}
+
+async function init() {
+    if (!await exists(".git")) {
+        await shell(`git init .`);
+        await shell(`git remote add origin ${REPO_URL}`);
+    }
+}
+
+async function sync() {
+    if (await shell("git fetch")) {
+        if (await shell("git checkout --detach -f origin/sass")) {
+            // await shell("git status");
+            await shell("ls -lR");
+            return true;
+        }
+    }
+
+    return false;
+}
 
 async function loop() {
+    await init();
+    await sync();
+    await boot();
 
+    while (true) {
+        if (await sync()) {
+           await update();            
+        }
+
+        await wait();
+    }
 }
 
 loop();
